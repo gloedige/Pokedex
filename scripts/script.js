f2e7f34aea30920edc389f7df963279855c13de2
gloedige <gerd.loedige@posteo.de>
2025-10-01 15:25:41 +0200

    update script with eventlistener for form, create initDOMContentEventListener, fetchSinglePokemon, renderAllPokemons

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,32 @@
-let formSearchPokemon = document.getElementById('formSearchPokemon');
-formSearchPokemon.addEventListener('submit', function(){
-    // event.preventDefault();
-    findPokemonByName();
-});
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon"
+
+function initFormFieldEventListener(){
+    let formSearchPokemon = document.getElementById('formSearchPokemon');
+    if (formSearchPokemon){
+        formSearchPokemon.addEventListener('submit', function(event){
+            handleFormSubmit(event);
+            // schiebe die Funktion "findPokemonByName()" nach handleFormSubmit
+        })
+    };
+}
+
+function initDOMContentEventListener(){
+    let path = "/1";
+    // initDialog();   
+    initFormFieldEventListener();
+    fetchSinglePokemon(path);
+}
+
+async function fetchSinglePokemon(path="") {
+    let response = await fetch(BASE_URL + path)
+    let responseToJson = await response.json();
+    console.log(responseToJson);
+    renderAllPokemons(responseToJson);
+}
+
+function renderAllPokemons(singlePokemon){
+    let allPokemonsRef = document.getElementById('container_pokemons');
+    allPokemonsRef.innerHTML = renderSinglePokemon(singlePokemon);
+}
+
+document.addEventListener('DOMContentLoaded', initDOMContentEventListener);
