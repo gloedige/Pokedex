ffffe8b421811cd269ef0c3d465c7dfb99e8f48c
gloedige <gerd.loedige@posteo.de>
2025-10-02 20:41:52 +0200

    update IMG_URL with capital letter, insert funktion to calc fix number of digits in ID

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,15 +1,15 @@
-let imgURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/";
+const IMG_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/";
 
 function renderSinglePokemon(singlePokemon){
     return  `
             <li class="animating">
                 <a href="">
-                    <img src = "${imgURL}${singlePokemon.id}.svg" alt="Pokemon image" class="poke_img">
+                    <img src = "${IMG_URL}${singlePokemon.id}.svg" alt="Pokemon image" class="poke_img">
                 </a>
                 <div class="pokemon_info">
                     <p class="id">
                         <span class="number_prefix">Nr.&nbsp;</span>
-                        000${singlePokemon.id}
+                        ${String(singlePokemon.id).padStart(4,'0')}
                     </p>
                     <h5>${(singlePokemon.name).toUpperCase()}</h5>
                     <div class="abilities">
